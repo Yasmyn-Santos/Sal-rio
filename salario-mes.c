528f9e776c10cd3c547838ddc5e99e08f56f714c
Yasmyn Santos <108375544+Yasmyn-Santos@users.noreply.github.com>
2022-09-28 21:00:26 -0300

    Update salario-mes.c

--- salario-mes.c
+++ salario-mes.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
-*/ Escreva um programa que leia o número de um funcionário, seu número de horas trabalhadas, o valor que recebe por hora e calcula o salário desse funcionário. 
+
+/* Escreva um programa que leia o número de um funcionário, seu número de horas trabalhadas, o valor que recebe por hora e calcula o salário desse funcionário. 
 A seguir, mostre o número e o salário do funcionário, com duas casas decimais.
 
 Entrada
@@ -8,7 +9,7 @@ funcionário recebe por hora trabalhada, respectivamente.
 
 Saída
 Imprima o número e o salário do funcionário, conforme exemplo fornecido, com um espaço em branco antes e depois da igualdade. No caso do salário, 
-também deve haver um espaço em branco após o $. /*
+também deve haver um espaço em branco após o $. */
 
 int main() {
 
